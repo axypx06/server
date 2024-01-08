18dfef2538df3ecf82e69271c8de6c84aaf7eeeb
PRIYANSHI RAWAT <110666919+axypx06@users.noreply.github.com>
2024-01-08 23:53:38 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(cookieParser());
+  const allowedOrigins = ['http://localhost:3001'];
+  app.enableCors({
+    origin:allowedOrigins, // Specify the allowed origin(s)
+    methods: 'GET,PUT,POST,DELETE', // Specify which methods are allowed
+    allowedHeaders: 'Content-Type,Authorization', // Specify allowed headers
+    credentials: true, // Allow sending cookies or other credentials
+  }
+  );
+  await app.listen(10000);
 }
 bootstrap();
