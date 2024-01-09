095567a442932b80255b31bce4818c9a3bec75bf
PRIYANSHI RAWAT <110666919+axypx06@users.noreply.github.com>
2024-01-09 14:10:01 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,26 +8,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+   const allowedOrigins = ['http://localhost:3000', 'https://telebotclient6.onrender.com'];
   // Adjust CORS configuration for webhook
   app.enableCors({
-    origin: '*', // Allow any origin since the Telegram API will send requests
-    methods: 'POST', // Allow only POST method for incoming updates
-    allowedHeaders: 'Content-Type', // Specify allowed headers
+    origin:allowedOrigins, // Specify the allowed origin(s)
+    methods: 'GET,PUT,POST,DELETE', // Specify which methods are allowed
+    allowedHeaders: 'Content-Type,Authorization', // Specify allowed headers
     credentials: true, // Allow sending cookies or other credentials
-  });
+  }
+  );
 
-  // Get the Telegram bot token from environment variables
-  const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
-
-  // Set up the webhook URL
-  const webhookUrl = `/${TELEGRAM_BOT_TOKEN}/webhook`; // Adjust the URL as needed
-
-  // Enable the webhook and specify the route
-  await app.setGlobalPrefix(webhookUrl);
-
+ 
   // Start the NestJS application
-  await app.listen(process.env.PORT || 1000);
+  await app.listen(process.env.PORT);
 }
 
 bootstrap();
